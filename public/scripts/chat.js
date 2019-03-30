9d6500257716e50473243b64b799a7a331b72a95
Sleightly <ejc042@ucsd.edu>
2019-03-30 15:04:32 -0700

    finished chat

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,11 +1,52 @@
+//Create chat room
+function chatRoom(user1, user2) {
+	window.location.href = "dm.html?p1="+user1+"&p2="+user2;
+}
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+function getUrlVar(name) {
+    return getUrlVars()[name];
+}
+
+//authenticate
+function authenticate() {
+	var user1 = getUrlVars('p1');
+	var user2 = getUrlVars('p2');
+	var myid = getUniqueId();
+	if (myid != user1 && myid != user2) {
+		window.location.href = 'home.html';
+	}
+	if (myid == user1) {
+		//get user2
+		var name = firebase.firestore().collection('users').doc(user2).name;
+		document.getElementById("otherName").innerText = name;
+	} else {
+		var name = firebase.firestore().collection('users').doc(user1).name;
+		document.getElementById("otherName").innerText = name;
+	}
+	loadMessages();
+}
+
+
 // Saves a new message on the Cloud Firestore.
 function saveMessage(messageText, user1, user2) {
   var uniqueId = getId(user1, user2);
   // Add a new message entry to the Firebase database.
   return firebase.firestore().collection('chat').doc(uniqueId)
     .set({
-    to: user1,
-    from: user2,
+    from: user1,
+    to: user2,
     text: messageText,
     timestamp: firebase.firestore.FieldValue.serverTimestamp()
   }).catch(function(error) {
@@ -16,19 +57,38 @@ function saveMessage(messageText, user1, user2) {
 // Loads chat messages history and listens for upcoming ones.
 function loadMessages(user1, user2) {
   var uniqueId = getId(user1, user2);
+  var iamone = true;
+
+  var myid = getUniqueId();
+  if (user1 != myid) {
+  	iamone = false;
+  }
 
   var query = firebase.firestore().collection('chat').doc(uniqueId).orderBy('timestamp', 'desc').limit(12);
   
   // Start listening to the query.
   query.onSnapshot(function(snapshot) {
     snapshot.docChanges().forEach(function(change) {
-      if (change.type === 'removed') {
-        deleteMessage(change.doc.id);
-      } else {
         var message = change.doc.data();
-        displayMessage(change.doc.id, message.timestamp, message.name,
-                      message.text);
-      }
+        var texts = document.getElementsByClassName('dm-body')[0];
+        var para = document.createElement("p");
+		para.innerText = message.text;
+		var div = document.createElement("div");
+		div.appendChild(para);
+        if (message.from == user1) {
+        	if (iamone) {
+        		div.classList.add("speech-bubble-out");
+        	} else {
+        		div.classList.add("speech-bubble-in");
+        	}
+        } else {
+        	if (iamone) {
+        		div.classList.add("speech-bubble-in");
+        	} else {
+        		div.classList.add("speech-bubble-out");
+        	}
+        }
+        texts.appendChild(div);
     });
   });
 }
@@ -50,3 +110,6 @@ function getUniqueId() {
 function getOtherId() {
   //get other id
 }
+
+saveMessage()
+loadMessages()
