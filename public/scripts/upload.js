1a23779e3acdab4097a2ea4a7de655ecc75a82c8
Sleightly <ejc042@ucsd.edu>
2019-03-30 20:54:12 -0700

    upload for photos

---
+++ public/scripts/upload.js
@@ -0,0 +1,16 @@
+function uploadPhoto(pic, title, type, user) {
+	ref = firebase.storage().ref();
+	const name = (+new Date()) + '-' + pic.name;
+	var link;
+	const task = ref.child(name).put(file)
+	task.then(snapshot => snapshot.ref.getDownloadURL())
+	.then(function(url) => link = url)
+
+	firebase.firestore().collection('items').set({
+		"title": title,
+		"types": type,
+		"userId": user,
+		"imageUrl": link  
+	});
+
+}
